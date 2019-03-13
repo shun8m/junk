9e8fd1c3a0628773b28cb9516903475965eaf8ba
Shunya Matsumoto <shun.code@gmail.com>
2019-03-13 23:26:05 +0900

    microtimeはマイクロ秒

--- PHP/binarysearch.php
+++ PHP/binarysearch.php
@@ -6,11 +6,12 @@ $firstIndex = 0;
 $lastIndex = $listCount - 1;
 $isFind = false;
 $startTime = microtime(true);
+echo "開始時間: ".date('Y-m-d H:i:s',(int)$startTime)."\n";
 do {
     $centerIndex = (Int) (($firstIndex + $lastIndex) / 2);
     if ($list[$centerIndex] == SearchObject) {
         $isFind = true;
-        echo '見つかったンゴ　キーは' . $centerIndex . 'です';
+        echo "見つかったンゴ　キーは" . $centerIndex . "です\n";
         break;
     } else if ($list[$centerIndex] < SearchObject){
         $firstIndex = $centerIndex + 1;
@@ -19,8 +20,9 @@ do {
     }
 } while($firstIndex <= $lastIndex);
 if (!$isFind) {
-    echo '見つからないんだよなあ・・・';
+    echo "見つからないんだよなあ・・・\n";
 }
 $endTime = microtime(true);
+echo "終了時間: ".date('Y-m-d H:i:s',(int)$endTime)."\n";
 $resultTime = $endTime - $startTime;
-echo "\n処理時間は" . $resultTime . "みり秒だゾ";
+echo "処理時間は" . sprintf('%0.5f', $resultTime). "秒だゾ";
